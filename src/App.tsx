bde94e9b6e9ff58a41dfca4b7c1cb6791403fbad
waldekmastykarz <waldek@mastykarz.nl>
2020-10-28 08:59:31 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Providers, ProviderState } from '@microsoft/mgt';
+import { Agenda, Login } from '@microsoft/mgt-react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  Providers.globalProvider.onStateChanged(e => {
+    if (Providers.globalProvider.state !== ProviderState.Loading) {
+      setIsLoggedIn(Providers.globalProvider.state === ProviderState.SignedIn);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Login />
       </header>
+      <div>
+        {isLoggedIn &&
+          <Agenda />}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
+import { MsalProvider, Providers } from '@microsoft/mgt';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+Providers.globalProvider = new MsalProvider({
+  clientId: 'd7cb93c9-9097-4e38-8f06-7c0088ac3318',
+  scopes: ['calendars.read', 'user.read', 'openid', 'profile', 'people.read', 'user.readbasic.all']
+});
 
 ReactDOM.render(
   <React.StrictMode>
